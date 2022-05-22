7818543a0b5509c33b2a9bda3ab4e7062b4b2a3c
Nhlamulo Khosa <nhlamulokhosa21@gmail.com>
2022-05-22 09:00:06 +0200

    added flash messages to alert player sign up and login status

--- modules/accountData.js
+++ modules/accountData.js
@@ -1,24 +1,24 @@
-'use strict'
+"use strict";
 
-const Players=[]
+const Players = [];
 
-module.exports={
-add: function(player)
-    {
-    Players.push(player)    
-    },
+module.exports = {
+  add: function (player) {
+    Players.push(player);
+  },
 
-alreadyExists: function(email, username){
-    
-    return Players.find((elem)=>email===elem.email ||username===elem.username)
+  alreadyExists: function (email, username) {
+    return Players.find(
+      (elem) => email === elem.email || username === elem.username
+    );
+  },
 
-},
-
-verify: function(username,password)
-{
-    return Players.find((elem)=>username===elem.username && password===elem.password)
-},
-    getData: function(){
-        return Players
-    }
-}
+  verify: function (username, password) {
+    return Players.find(
+      (elem) => username === elem.username && password === elem.password
+    );
+  },
+  getData: function () {
+    return Players;
+  },
+};
--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,70 +1,67 @@
-'use strict'
-const path =require('path')
-const express = require('express')
-const router=express.Router()
-const userData= require('../modules/accountData')
+"use strict";
+const path = require("path");
+const express = require("express");
+const router = express.Router();
+const userData = require("../modules/accountData");
+router.get("/", function (req, res) {
+  res.render("home");
+});
 
-router.get('/', function(req,res){
-    res.sendFile(path.join(__dirname ,'../views/home.html'))
-})
+router.get("/register", function (req, res) {
+  res.render("register");
+});
 
-router.get('/register', function(req, res){
-    res.sendFile(path.join(__dirname,'../views/register.html'))
-})
-
-router.get('/login', function(req, res){
-    res.sendFile(path.join(__dirname, '../views/login.html'))
-})
+router.get("/login", function (req, res) {
+  res.render("login");
+});
 
 //RESTful api
 
-router.get('/api/info', function(req, res){
-    res.json(userData.getData()) //respond with JSON
-})
+router.get("/api/info", function (req, res) {
+  res.json(userData.getData()); //respond with JSON
+});
 
-//routes for POST requests 
-router.post('/api/register', function(req,res){
-    console.log(`added player info: username ${req.body.username} password ${req.body.password} email ${req.body.email}`)
-    const exist=userData.alreadyExists(req.body.email, req.body.username)
-     console.log(`Register ${exist}`)
+//routes for POST requests
+router.post("/api/register", function (req, res) {
+  console.log(
+    `added player info: username ${req.body.username} password ${req.body.password} email ${req.body.email}`
+  );
+  const exist = userData.alreadyExists(req.body.email, req.body.username);
+  console.log(`Register ${exist}`);
 
-if(!exist)
-{
-    const player= {
-        username: req.body.username,
-        password: req.body.password,
-        email: req.body.email
-        }
-        userData.add(player)
-        console.log('users',userData.getData())
-        res.send("<div align ='center'><h2>Registration successful</h2></div> <br><br><div align ='center'><a href='/account/login'>Login</a></div>")
+  if (!exist) {
+    const player = {
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+    };
+    userData.add(player);
+    console.log("users", userData.getData());
+    req.flash('success', 'Registration successful, Login to your account');
+    res.redirect(req.baseUrl + "/login");
+    
+  } else {
+    // res.send("<div align ='center'><h2>Email already used</h2></div>")
+    req.flash('error', 'Account already exist, Login to your account'); 
+    res.redirect(req.baseUrl + "/login");
+  }
+});
 
-}
-else{
-   // res.send("<div align ='center'><h2>Email already used</h2></div>")
-     res.redirect(req.baseUrl +'/login')
-}
+router.post("/api/login", function (req, res) {
+  console.log(
+    `login credentials :  usernamer ${req.body.password} email ${req.body.username}`
+  );
 
+  const exist = userData.verify(req.body.username, req.body.password);
+  console.log(`Login ${exist}`);
 
-})
+  if (exist) {
+    req.flash('success', 'Login successful');
+    res.redirect(req.baseUrl+"/register")
+  } else {
+    req.flash('error', 'Account does not exist, register instead'); 
+    res.redirect(req.baseUrl + "/register");
+  }
+});
 
-router.post('/api/login', function(req, res){
-
-   console.log(`login credentials :  usernamer ${req.body.password} email ${req.body.username}`)
-  
-
-   const exist=userData.verify(req.body.username, req.body.password)
-   console.log(`Login ${exist}`)
-
-   if(exist){
-    res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${req.body.username}</h3></div><br><br><div align='center'><a href='/account/'>logout</a></div>`);
-  
-   }
-   else{
-    res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='/account/login'>login again</a></div>  <br><br><div align ='center'><a href='/account/register'>Register</a></div>");
-
-   }
-
-})
-
-module.exports=  router 
+module.exports = router;
