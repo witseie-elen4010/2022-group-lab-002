c45483af73d1b001224c346e69d697698b882b03
Mandla Khumalo <1574406@students.wits.ac.za>
2022-05-23 19:38:01 +0200

    Removed unnecessary files

--- __tests__/scoreTests.test.js
+++ __tests__/scoreTests.test.js
@@ -2,10 +2,7 @@
 
 const gameScore = require('../modules/scoreData.js')
 test('Score do not display if the player has not guessed any word yet', () => {
-  
-  //let data = gameScore.getData()
   gameScore.add(2)
-
   expect(gameScore.getData().length).toBe(1)
 })
 
