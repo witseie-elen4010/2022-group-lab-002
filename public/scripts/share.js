491180dd06daa7797c3edb780189638993b2dee8
Ndeme <1891020@students.wits.ac.za>
2022-05-27 03:29:14 +0200

    invitation and score buttons active set on js-page

--- public/scripts/share.js
+++ public/scripts/share.js
@@ -3,10 +3,12 @@
 const twitter = document.getElementById('twitter')
 const telegram = document.getElementById('telegram')
 const whatsapp = document.getElementById('whatsapp')
-const showScore = document.getElementById('showScore')
-const pageUrl = location.href
-console.log(pageUrl) // Reference of PC
 
+const invitation = document.getElementById('invitation')
+
+const pageUrl = location.href // Responsible for saving the current page to the pageUrl
+
+const gameName = 'Extreme Worlde Game Score Board: '
 
 const numberOfWins = 0
 const numberOfWinsText = `The Player Won: ${numberOfWins}`
@@ -17,13 +19,20 @@ const numberOfTriesText = `Player's Attempts: ${numberOfTries}`
 const totalScore = 0
 const totalScoreText = `Player's Total Score: ${totalScore}`
 
+const invitationMessage = `Hey There!!! Come Try your luck in the Extreme Worlde Game... Can you guess my 5 Letter Word??? \n Link: \n ${pageUrl}`
 const scoreBoard = ` ${gameName} \n ${numberOfWinsText} \n ${numberOfTriesText} \n ${totalScoreText} \n Link: \n ${pageUrl}`
 let message = scoreBoard
+
+invitation.addEventListener('click', () =>{
+  console.log('invitation button clicked')
+  message = invitationMessage
+})
 
 showScore.addEventListener('click', () =>{
   console.log('showScore button clicked')
   message = scoreBoard
 })
+
 
 telegram.addEventListener('click', () => {
   console.log('twitter button clicked')
@@ -40,9 +49,11 @@ whatsapp.addEventListener('click', () => {
   window.open(`https://wa.me/?text= ${message} `)
 })
 
-let scoreArray = [numberOfWins,numberOfTries,totalScore]
+message = scoreBoard
 
-function scoreArrayFunction(){
+const scoreArray = [numberOfWins, numberOfTries, totalScore]
+
+function scoreArrayFunction () {
   return scoreArray
 }
 
