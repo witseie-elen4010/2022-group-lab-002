ea650227d87a8d07aa19e866e233ac93552460bf
Nhlamulo Khosa <nhlamulokhosa21@gmail.com>
2022-05-31 01:52:22 +0200

    fix database code making the site down

--- database/config/db.js
+++ database/config/db.js
@@ -1,15 +1,16 @@
 const path = require("path");
 const dotenv = require('dotenv').config({ path: path.resolve(__dirname, '../../.env') })
 const mongoose = require('mongoose')
-const uriString= process.env.ATLAS_URI;
-console.log('here...', path.resolve(__dirname, '../../.env'), uriString)
+//const uriString= process.env.ATLAS_URI;
+const ATLAS_URI="mongodb+srv://Mulow21:1234%40Mulow@extreme-wordle.w2l6awc.mongodb.net/playersAccount?retryWrites=true&w=majoritygit" 
+//console.log('here...', path.resolve(__dirname, '../../.env'), uriString)
 
 const connectDb = async () =>{
     try{
-        const conn= await mongoose.connect(uriString)
+        const conn= await mongoose.connect(ATLAS_URI)
 
         console.log('MongoDB Connected: ${conn.connection.host')
-        console.log(uriString)
+        console.log(ATLAS_URI)
     }
     catch (error){
         console.log(error)
