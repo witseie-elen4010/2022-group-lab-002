0b5c09e9dfc3b2fded356a4a0e8b99742c90ddf5
SimbongileKiviet <lonelekiviet@gmail.com>
2022-05-31 18:30:01 +0200

    fixing the toggle issue

--- public/scripts/settings.js
+++ public/scripts/settings.js
@@ -1,17 +1,17 @@
+const settings = document.getElementsByClassName('contentBx')
+for (i = 0; i < settings.length; i++) {
+  settings[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}
 function changeColor (Color) {
   if (Color === 'white') {
-    document.body.classList.toggle('white');
+    document.body.className = 'white';
   }
   if (Color === 'black') {
-    document.body.classList.toggle('black');
+    document.body.className = 'black';
   }
   if (Color === 'grey') {
-    document.body.classList.toggle('grey');
+    document.body.className = 'grey';
   }
 }
-  const settings = document.getElementsByClassName('contentBx')
-  for (i = 0; i < settings.length; i++) {
-    settings[i].addEventListener('click', function () {
-      this.classList.toggle('active')
-    })
-  }
