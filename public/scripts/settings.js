be74f241eeb7ae57632b47a3b8de635a2a9f45ae
Simbongile Kiviet <68307273+SimbongileKiviet@users.noreply.github.com>
2022-05-31 19:13:21 +0200

    Revert "game settings bug updated"

--- public/scripts/settings.js
+++ public/scripts/settings.js
@@ -1,17 +1,11 @@
-const settings = document.getElementsByClassName('contentBx')
-for (i = 0; i < settings.length; i++) {
-  settings[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
-}
-function changeColor (Color) {
-  if (Color === 'white') {
-    document.body.className = 'white';
+function changeColor(Color) {
+    if (Color === 'white') {
+        document.getElementById("body").className = "white";
+      }
+      if (Color === 'black') {
+        document.getElementById("body").className = "black";
+      } 
+      if (Color === 'grey') {
+        document.getElementById("body").className = "grey";
+      }
   }
-  if (Color === 'black') {
-    document.body.className = 'black';
-  }
-  if (Color === 'grey') {
-    document.body.className = 'grey';
-  }
-}
