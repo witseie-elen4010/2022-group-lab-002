792e2f23f31162f3c16cec8958b911b444a1f0c9
SimbongileKiviet <lonelekiviet@gmail.com>
2022-05-31 17:29:22 +0200

    Removing the id on the body tag

--- public/scripts/settings.js
+++ public/scripts/settings.js
@@ -1,14 +1,14 @@
-function changeColor(Color) {
-    if (Color === 'white') {
-        document.getElementById("body").className = "white";
-      }
-      if (Color === 'black') {
-        document.getElementById("body").className = "black";
-      } 
-      if (Color === 'grey') {
-        document.getElementById("body").className = "grey";
-      }
+function changeColor (Color) {
+  if (Color === 'white') {
+    document.body.classList.toggle('white');
   }
+  if (Color === 'black') {
+    document.body.classList.toggle('black');
+  }
+  if (Color === 'grey') {
+    document.body.classList.toggle('grey');
+  }
+}
   const settings = document.getElementsByClassName('contentBx')
   for (i = 0; i < settings.length; i++) {
     settings[i].addEventListener('click', function () {
